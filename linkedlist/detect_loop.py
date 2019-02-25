ea62e2dd4927637d41588a6df22845c7180f5725
ssjsatish <ssj.satish@gmail.com>
2019-02-26 00:36:34 +0530

    added code to detect if there is loop in the list

---
+++ linkedlist/detect_loop.py
@@ -0,0 +1,18 @@
+'''
+detect loop if there is any in a given linked list
+'''
+class Node:
+    def __init__(self, new_data):
+        self.data = new_data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def push(self, new_data):
+
+        
+    def detectLoop(self, node):
+        return True
+    
