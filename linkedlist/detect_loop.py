677d2468ee457eddcc816c52a958ff1218b38da1
ssjsatish <ssj.satish@gmail.com>
2019-02-27 00:17:04 +0530

    loop detection code

--- linkedlist/detect_loop.py
+++ linkedlist/detect_loop.py
@@ -14,7 +14,54 @@ class LinkedList:
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
-        
-    def detectLoop(self, node):
-        return True
+    
+    # this method is used to detect the loop 
+    # in a given linked list using hashing
+    def detectLoopHashMethod(self):
+        h = set()
+        temp = self.head
+        while(temp):
+            if temp in h:
+                return True
+            h.add(temp)
+            temp = temp.next
+        return False
+
+    # detect loop in the list using floyd's cycle detection algorithm
+    def detectLoopFloydMethod(self):
+        slow = self.head
+        fast = self.head
+        while(slow and fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+    def print_list(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, ' -> ',end=' ')
+            temp = temp.next
+        print()
+
+
+if __name__=='__main__':
+    llist = LinkedList()
+    llist.push(10)
+    llist.push(20)
+    llist.push(30)
+    llist.push(40)
+    llist.head.next.next.next.next = llist.head
+    if(llist.detectLoopFloydMethod()):
+        print('Has loop')
+    else:
+        print('Doesnt have loop')
+
+    if(llist.detectLoopHashMethod()):
+        print('list has loop')
+    else:
+        print('list Does not have loop')
+
+
     
