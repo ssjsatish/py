e7e6646f10f81cd4ae63b9c1b555f09750295a73
satish <ssj.satish@gmail.com>
2019-03-18 01:05:14 +0530

    max width of a tree

--- tree/max_width.py
+++ tree/max_width.py
@@ -5,4 +5,28 @@ class TreeNode:
         self.right = None
 def getMaxWidth(root):
     maxWidth = 0
-    h = height()
+    h = height(root)
+    for i in range(1, h+1):
+        width = getMaxWidth(root, i)
+        if width > maxWidth:
+            maxWidth = width
+    return maxWidth
+
+def height(root):
+    if root is None:
+        return 0
+    else:
+        lheight = height(root.left)
+        rheight = height(root.right)
+    return (lheight+1) if lheight > rheight else (rheight+1)
+
+root = TreeNode(1) 
+root.left = TreeNode(2) 
+root.right = TreeNode(3) 
+root.left.left = TreeNode(4) 
+root.left.right = TreeNode(5) 
+root.right.right = TreeNode(8) 
+root.right.right.left = TreeNode(6) 
+root.right.right.right = TreeNode(7) 
+
+print(getMaxWidth(root))
