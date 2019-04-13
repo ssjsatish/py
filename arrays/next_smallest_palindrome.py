a1d9d791c2b1f250c68c314184c668645a9b43dc
satish <ssj.satish@gmail.com>
2019-04-13 12:03:56 +0530

    next smallest palindrome of a given number formed by array digits

---
+++ arrays/next_smallest_palindrome.py
@@ -0,0 +1,11 @@
+'''
+Given a number, in the form of an array a[] containing digits from 1 to 9(inclusive). 
+The task is to find the next smallest palindrome larger than this number.
+Input: n and followed by an array of digit of length n
+11
+9 4 1 8 7 9 7 8 3 2 2
+
+Output:
+9 4 1 8 8 0 8 8 1 4 9
+'''
+
