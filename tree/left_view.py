9c9e043ab0cef176f91a88ee98cf99fdff99ae8e
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-05-11 01:00:52 +0530

    left view of binary tree

--- tree/left_view.py
+++ tree/left_view.py
@@ -4,11 +4,23 @@ class TreeNode:
         self.right = None
         self.data = new_data
     
-def tree_left_view(root):
+def leftUtil(root, level, max_level):
     if root is None:
         return
-    else:
-        return root.data
+    if max_level[0] < level:
+        print(root.data, end=' ')
+        max_level[0] = level
+    leftUtil(root.left, level+1, max_level)
+    leftUtil(root.right, level+1, max_level)
+def printLeftView(root):
+    max_level = [0]
+    leftUtil(root,1,max_level)
 
 
 root = TreeNode(5)
+root.left = TreeNode(4)
+root.left.left = TreeNode(8)
+root.right = TreeNode(10)
+root.right = TreeNode(11)
+root.right.right = TreeNode(12)
+printLeftView(root)
